7c534f2c1130d0975db0f7155cfd90a25ed317a1
木村　翼 <tnryj505@gmail.com>
2022-04-18 17:33:49 +0900

    set vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,23 @@
 <template>
   <v-app>
     <v-toolbar app>
+      <v-toolbar-side-icon></v-toolbar-side-icon>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>マイアドレス帳</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
     </v-toolbar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
+    <v-content> </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  data () {
+  name: "App",
+  data() {
     return {
       //
-    }
-  }
-}
+    };
+  },
+};
 </script>
