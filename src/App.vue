87a7c9b79b71f6f9dfb12addef250cbae8360b16
木村　翼 <tnryj505@gmail.com>
2022-05-24 15:42:38 +0900

    add:メニューの開閉状態の管理

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <v-toolbar app>
-      <v-toolbar-side-icon @click="openSideMenu"></v-toolbar-side-icon>
+      <v-toolbar-side-icon @click="toggleSideMenu"></v-toolbar-side-icon>
       <v-toolbar-title class="headline text-uppercase">
         <span>マイアドレス帳</span>
       </v-toolbar-title>
@@ -14,6 +14,7 @@
 </template>
 
 <script>
+import { mapActions } from 'vuex'
 import SideNav from './components/SideNav'
 export default {
   name: "App",
@@ -26,9 +27,7 @@ export default {
     };
   },
   methods: {
-    openSideMenu(){
-      this.$store.dispatch('toggleSideMenu')
-    }
+    ...mapActions(['toggleSideMenu'])
   },
 };
 </script>
