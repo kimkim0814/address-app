76230ab7e8e26188acb743a2296f78507f611ee7
木村　翼 <tnryj505@gmail.com>
2022-05-25 16:46:19 +0900

    add:連絡先追加画面作成

--- src/views/Addresses.vue
+++ src/views/Addresses.vue
@@ -5,8 +5,14 @@
         <h1>連絡先一覧</h1>
       </v-flex>
 
+      <v-flex xs12 mt-5 text-xs-right>
+        <router-link :to="{ name: 'address_edit' }">
+          <v-btn color="info"> 連絡先追加 </v-btn>
+        </router-link>
+      </v-flex>
+
       <v-flex xs12 mt-5 justify-center>
-        <v-data-table :headers='headers' :items='addresses'>
+        <v-data-table :headers="headers" :items="addresses">
           <template v-slot:items="props">
             <td class="text-xs-left">{{ props.item.name }}</td>
             <td class="text-xs-left">{{ props.item.tel }}</td>
@@ -21,29 +27,29 @@
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       headers: [
-        { text: '名前', value: 'name' },
-        { text: '電話番号', value: 'tel' },
-        { text: 'メールアドレス', value: 'email' },
-        { text: '住所', value: 'address' }
+        { text: "名前", value: "name" },
+        { text: "電話番号", value: "tel" },
+        { text: "メールアドレス", value: "email" },
+        { text: "住所", value: "address" },
       ],
       addresses: [
         {
-          name: '友人１',
-          tel: '090-0000-1111',
-          email: 'sample1@mail.com',
-          address: '東京都渋谷区'
+          name: "友人１",
+          tel: "090-0000-1111",
+          email: "sample1@mail.com",
+          address: "東京都渋谷区",
         },
         {
-          name: '友人２',
-          tel: '090-2222-3333',
-          email: 'sample2@mail.com',
-          address: '東京都品川区'
-        }
-      ]
-    }
-  }
-}
+          name: "友人２",
+          tel: "090-2222-3333",
+          email: "sample2@mail.com",
+          address: "東京都品川区",
+        },
+      ],
+    };
+  },
+};
 </script>
